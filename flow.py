32626f1587617ffa17f092d2504b762e8b423bf5
ABhardwaj123 <agastya.bhardwajdps@gmail.com>
2026-09-08 19:36:09 +0530

    full working

--- flow.py
+++ flow.py
@@ -27,3 +27,65 @@ tools = [simple_screener]
 llm_with_tools = llm.bind_tools(tools)
 
 tool_node = ToolNode(tools)
+
+#state is the info the graph is carrying while it executes
+#has all the info that gets passed between the nodes of the graph
+#used to store kind of previous-context
+
+#dict means that our state will look something like this
+# state = {"messages": [....]}
+class State(dict):  
+    #whenever this field gets updated , use add_messages to determin how new messages should be combined with others
+    messages: Annotated[list , add_messages]
+
+#this is a langGraph node. receives current state , gives the state to LLM and returns the LLM'S response as an update to state
+#chatbot is expecting input of state
+def chatbot(state: State):
+    #take the current state , sends that to the LLM and then takes the LLM's response and returns it as a new message to be added to the state
+    return {"messages": [llm_with_tools.invoke(state["messages"])]}
+
+
+def router(state: State):
+    #getting the last message of the conversation because that is the response the chatbot just generated
+    last_message = state["messages"][-1]
+
+    #checks if the LLM has asked for a tool or not because either LLM can ask for a tool or it can directly answer on its own without using tool
+
+    #we are checking if the last message have tool calls and if tes , then are there actually any tool calls
+    if hasattr(last_message , "tool_calls") and last_message.tool_calls:
+        return "tools"
+    else:
+        return END
+
+
+#graph_builder is just used to describe the graph
+#creating a graph whose state follows State
+graph_builder = StateGraph(State)
+graph_builder.add_node("chatbot" , chatbot)
+graph_builder.add_node("tools" , tool_node)
+graph_builder.add_edge(START , "chatbot")
+graph_builder.add_edge("tools", "chatbot")
+
+#after chatbot is done , router will decide what will happen next
+graph_builder.add_conditional_edges("chatbot", router)
+
+
+#this is used to store checkpoints of the graph's state in memory
+#stores graph-state checkpoints in memory so the state can be persisted across graph execution
+memory = InMemorySaver()
+
+#this is where the graph actually becomes an executable one
+#passing checkpointer so that when this graph runs , this checkpointer to be used to save state
+graph = graph_builder.compile(checkpointer=memory)
+
+
+
+if __name__ == "__main__":
+    while True:
+        prompt = input("Pass your prompt here: ")
+
+        #now executing the graph with initial state as {"messages": [{"role":"user" , "content":prompt}]}
+        #we need configuration to tell langGraph to save checkpoints for this execution thread
+        #thread_id identifies which conversation this state belongs to
+        result = graph.invoke({"messages":[{"role":"user" , "content":prompt}]}, config={"configurable":{"thread_id":1234}})
+        print(result["messages"][-1].content)
